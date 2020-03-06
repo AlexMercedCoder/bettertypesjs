505b81c10d6bdb92c7ad5f83bf9ae2e15b12fb0b
Alex Merced <alexmerced@alexmerced.com>
2020-03-06 09:40:36 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,334 @@
+const fetch = require('node-fetch');
+
+////////////////////////
+//Random Number Functions
+////////////////////////
+
+const randomNumber = (num) => {
+    return Math.floor(Math.random() * num);
+};
+
+const randomIndex = (arr) => {
+    return randomNumber(arr.length);
+};
+
+const randomRange = (low, high) => {
+    let num = low - 1;
+    while (num < low || num > high) {
+        num = randomNumber(high);
+    }
+    return num;
+};
+
+const Random = {
+    number: randomNumber,
+    index: randomIndex,
+    range: randomRange
+};
+
+/////////////////
+//Fetcher class
+//Purpose: Api fetching Utility
+/////////////////
+
+class Fetcher {
+    constructor(url) {
+        this.url = url;
+    }
+
+    call = async (callback, config, urlExt = '') => {
+        const response = await fetch(`${this.url}${urlExt}`, config);
+        const json = await response.json();
+        this.lastCall = await json;
+        await callback(json);
+    };
+
+    recall = () => {
+        return this.lastCall;
+    };
+}
+
+/////////////////
+//BetterSet
+//Purpose: Leveraging the benefits of sets and arrays
+/////////////////
+
+class BetterSet {
+    constructor(arr) {
+        this.arr = [...new Set(arr)];
+        this.set = new Set(arr);
+    }
+
+    arrMeth(method, argument) {
+        const result = eval(`this.arr.${method}(${argument})`);
+        this.arr = [...new Set(this.arr)];
+        this.set = new Set(this.arr);
+        return result;
+    }
+
+    isSuperset(subset) {
+        for (let elem of subset.set) {
+            if (!this.set.has(elem)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    union(setB) {
+        return new PowerSet([...this.set, ...setB.set]);
+    }
+
+    intersection(setB) {
+        let _intersection = new Set();
+        for (let elem of setB.set) {
+            if (this.set.has(elem)) {
+                _intersection.add(elem);
+            }
+        }
+        return new PowerSet([..._intersection]);
+    }
+
+    symmetricDifference(setB) {
+        let _difference = new Set(this.set);
+        for (let elem of setB.set) {
+            if (_difference.has(elem)) {
+                _difference.delete(elem);
+            } else {
+                _difference.add(elem);
+            }
+        }
+        return new PowerSet([..._difference]);
+    }
+
+    difference(setB) {
+        let _difference = new Set(this.set);
+        for (let elem of setB.set) {
+            _difference.delete(elem);
+        }
+        return new PowerSet([..._difference]);
+    }
+}
+
+///////////////////
+//BetterString
+//////////////////
+
+const BetterString = () => {
+    String.prototype.words = function() {
+        return this.split(' ');
+    };
+
+    String.prototype.reverseWords = function() {
+        return this.split(' ')
+            .reverse()
+            .join(' ');
+    };
+
+    String.prototype.swapWord = function(target, replace) {
+        const theString = this.split(' ');
+        const theIndex = theString.findIndex((value) => value === target);
+        theString[theIndex] = replace;
+        return theString.join(' ');
+    };
+};
+
+///////////////////
+//BetterArray
+///////////////////
+class BetterArray {
+    constructor(arr) {
+        this.arr = arr;
+    }
+
+    arrMeth(method, argument) {
+        const result = eval(`this.arr.${method}(${argument})`);
+        return [this, result];
+    }
+
+    random() {
+        const randomIndex = Math.floor(Math.random() * this.length);
+
+        const RandomValue = this.arr[randomIndex];
+        return RandomValue;
+    }
+
+    remove(callback) {
+        const newArray = [];
+        for (i = 0; i < this.arr.length; i++) {
+            if (callback(this.arr[i], i)) {
+            } else {
+                newArray.push(this.arr[i]);
+            }
+        }
+        return new BetterArray(newArray);
+    }
+
+    undupe() {
+        return [...new Set(this.arr)];
+    }
+
+    randElim() {
+        return this.arr.splice(randomIndex(this.arr), 1);
+    }
+
+    leaveOne() {
+        const newArray = [];
+        while (this.arr.length > 1) {
+            newArray.push(this.arr.randElim()[0]);
+        }
+        return new BetterArray(newArray);
+    }
+
+    leaveSome(num) {
+        const newArray = [];
+        while (this.arr.length > num) {
+            newArray.push(this.arr.randElim()[0]);
+        }
+        return new BetterArray(newArray);
+    }
+
+    findRemove(value) {
+        return this.arr.splice(this.arr.findIndex((val) => val === value), 1);
+    }
+
+    addLength(length, value) {
+        while (this.arr.length < length) {
+            this.arr.push(value);
+        }
+        return this;
+    }
+
+    lessLengthRight(length) {
+        const removed = [];
+        while (this.arr.length > length) {
+            removed.push(this.arr.pop());
+        }
+        return removed;
+    }
+
+    lessLengthLeft(length) {
+        const removed = [];
+        while (this.arr.length > length) {
+            removed.push(this.arr.shift());
+        }
+        return removed;
+    }
+
+    someMore(callback, number) {
+        let counter = this.arr.filter(callback).length;
+        return counter >= number;
+    }
+
+    everyLess(callback, number) {
+        let counter = this.remove(callback).arr.length;
+        return counter <= number;
+    }
+
+    mapToObject(callback) {
+        const newObj = {};
+        for (i = 0; i < this.arr.length; i++) {
+            const propToBe = callback(this.arr[i], i);
+            if (propToBe instanceof Array && propToBe.length >= 2) {
+                newObj[propToBe[0]] = propToBe[1];
+            }
+        }
+        return newObj;
+    }
+
+    mapToMap(callback) {
+        const newMap = new Map();
+        for (i = 0; i < this.arr.length; i++) {
+            const propToBe = callback(this.arr[i], i);
+            if (propToBe instanceof Array && propToBe.length >= 2) {
+                newMap.set(propToBe[0], propToBe[1]);
+            }
+        }
+        return newMap;
+    }
+
+    mapToSet(callback) {
+        const newSet = [];
+        for (i = 0; i < this.arr.length; i++) {
+            newSet.push(callback(this.arr[i], i));
+        }
+        return new Set(newSet);
+    }
+
+    mapToUnique(callback) {
+        const newSet = [];
+        for (i = 0; i < this.arr.length; i++) {
+            newSet.push(callback(this.arr[i], i));
+        }
+        return [...new Set(newSet)];
+    }
+
+    squish() {
+        const removedValues = [];
+        removedValues.push(this.arr.shift());
+        removedValues.push(this.arr.pop());
+        return [this, removedValues];
+    }
+
+    shuff() {
+        const original = [...this.arr];
+        const shuffled = [];
+        for (i = 0; i < this.arr.length; i++) {
+            shuffled.push(original.randElim());
+        }
+
+        return new BetterArray(shuffled.flat());
+    }
+
+    toStrings() {
+        return new BetterArray(this.arr.map((value) => String(value)));
+    }
+
+    toNums() {
+        return new BetterArray(this.arr.map((value) => Number(value)));
+    }
+
+    toBools() {
+        return new BetterArray(this.arr.map((value) => Boolean(value)));
+    }
+
+    iPop() {
+        const newArr = [...this.arr];
+        const removed = newArr.pop();
+        return [new BetterArray(newArr), removed];
+    }
+
+    iShift() {
+        const newArr = [...this.arr];
+        const removed = newArr.shift();
+        return [new BetterArray(newArr), removed];
+    }
+
+    iPush(value) {
+        const newArr = [...this.arr];
+        const added = newArr.push(value);
+        return [new BetterArray(newArr), added];
+    }
+
+    iUnshift(value) {
+        const newArr = [...this.arr];
+        const added = newArr.unshift(value);
+        return [new BetterArray(newArr), added];
+    }
+
+    iSplice(index, amount) {
+        const newArr = [...this.arr];
+        const removed = newArr.splice(index, amount);
+        return [new BetterArray(newArr), removed];
+    }
+}
+
+//THE exports
+
+module.exports = {
+    Random,
+    Fetcher,
+    BetterSet,
+    BetterString,
+    BetterArray
+};
---
+++ readme.md
@@ -0,0 +1,216 @@
+# Alex Merced's Utilities
+
+---
+
+## AlexMercedCoder.com
+
+This library contain some utility functions.
+
+### installation
+
+#### NPM
+
+npm i mercedutils
+
+```
+const {superArray, utils, superString} = require("mercedutils")
+
+superArray(); //add methods to array prototype
+superString(); //add methods to string prototype
+
+const {
+    randomIndex,
+    randomRange,
+    randomNumber,
+    eliminateDupes,
+    Fetcher,
+    SuperSet
+} = utils() // makes these utility functions available
+```
+
+#### Github
+
+git clone https://github.com/AlexMercedCoder/JS_Utilities.git
+
+```
+const {superArray, utils, superString} = require("superArray.js")
+
+superArray(); //add methods to array prototype
+superString(); //add methods to string prototype
+
+const {
+    randomIndex,
+    randomRange,
+    randomNumber,
+    eliminateDupes,
+    Fetcher,
+    SuperSet,
+    countWords,
+    highWordCount
+} = utils() // makes these utility functions available
+
+```
+
+---
+
+### superArray
+
+---
+
+**superArray() =>** run this function and going forward all the below methods will be available to all your arrays
+
+---
+
+**Array.random() =>** return random element from array
+
+**Array.remove((value, index) => return boolean)** => the opposite of filter, remove elements where the callback function returns true, returns a superArray
+
+**Array.undupe() =>** returns a superArray of the array with duplicates removed
+
+**Array.randElim() =>** eliminates a random element and returns it
+
+**Array.leaveOne()** => Randomly eliminates all but one element from array and returns a superArray of removed elements
+
+**Array.leaveSome(number)** => Randomly eliminates all but a defined number of elements from array and returns a superArray of removed elements
+
+**Array.findRemove(value)** => finds and removes value from array returning the removed value
+
+**Array.addLength(length, value)** => increases array to desired length and fills in additional spots with the value passed.
+
+**Array.lessLengthRight(length)** => removes elements from back of the array till is desired length, returns array of removed values
+
+**Array.lessLengthRight(length)** => removes elements from front of the array till is desired length, returns array of removed values
+
+**Array.someMore((value, index) => return boolean, number)** => returns true if the number of iterations that return true are equal or greater to the number argument
+
+**Array.everyLess((value, index) => return boolean, number)** => returns true if the number of iterations that return false are equal or less to the number argument
+
+**Array.MapToObject((value, index) => return [key, value])** => like map but returns an object, the callback function must return a two element array [key, value]
+
+**Array.MapToMap((value, index) => return [key, value])** => like map but returns an Map, the callback function must return a two element array [key, value]
+
+**Array.MapToMap((value, index) => return [key, value])** => like map but returns a Set
+
+**Array.MapToUnique((value, index) => return [key, value])** => like map but returns an Array of only unique elements
+
+**Array.squish()** => removes the first and last elements of the array and return them in an array
+
+**Array.shuff()** => return shuffled version of array
+
+**Array.toStrings()** => return array with all elements casted as strings
+
+**Array.toNums()** => return array with all elements casted as Numbers
+
+**Array.toBools()** => return array with all elements casted as Booleans
+
+**Array.iPop()** => immutable pop, return new version of array with last value popped
+
+**Array.iPush(value)** => immutable push, return new version of array with value pushed into end of array
+
+**Array.iShift()** => immutable shift, return new version of array with first value removed
+
+**Array.iUnshift(value)** => immutable unshift, return new version of array with value added at beginning of array
+
+**Array.iSplice(index, amount)** => immutable splice, return new version of array with the specified number of elements removed starting with the specified index.
+
+---
+
+### superString
+
+---
+
+**superString() =>** run this function and going forward all the below methods will be available to all your strings
+
+---
+
+**String.words() =>** returns array of words in the string
+
+**String.reverseWords() =>** returns string with the words reversed
+
+**String.swapWord(target, replacement) =>** returns string where the first instance of the target word is replaced by the replacement.
+
+---
+
+### Utility Functions
+
+---
+
+**randomIndex(Array) =>** Return random number 0 - Array.length
+
+**randomRange (Low, High) =>** Return number between low and high
+
+**randomNumber(Number) =>** Return random number 0 - Number
+
+**eliminateDupes(Array) =>** Return Array with duplicates removed
+
+**countWords(String) =>** returns object of words in string and their word count
+
+**highWordCount(string) =>** returns array with most used word and count in a string
+
+### Fetcher
+
+---
+
+The Fetcher class can help you encapsulate your API endpoints
+
+```
+const get = new Fetcher('http://www.xyz/get/')
+const post = new Fetcher('http://www.xyz/post/')
+const delete = new Fetcher('http://www.xyz/delete/')
+const update = new Fetcher('http://www.xyz/update/')
+```
+
+**call(callback,config,extUrl)**
+
+this function will call the url passed and run the callback function when the promise resolves and has been converted into JSON. You can pass in a config object include things like, headers, body, and specify type of call (defaults to get). The extUrl parameter will be appended to the url which allows you to get more mileage out of a single instance of fetcher. The resulting data from the most recent call is saved in case you need to access it again.
+
+```
+get.call((json) => console.log(json))
+
+post.call((json) => console.log(json), {
+        method: 'post',
+        body:    JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' },
+    }, "?user=SteveJones")
+
+```
+
+**recall()**
+returns the data from the most recent call
+
+---
+
+### PowerSet
+
+---
+
+combines the benefits of arrays and sets but giving you access to a dataset in both forms. The constructor takes an array to generate the PowerSet from. I implemented the methods from the MDN examples for sets, cause well... they are useful.
+
+```
+const { PowerSet } = utils();
+
+const pset1 = new PowerSet([1, 2, 3, 4]);
+const pset2 = new PowerSet([3, 4, 5, 6]);
+
+console.log(pset1.difference(pset2));
+```
+
+#### PowerSet Properties
+
+**this.set =>** the set of the array passed in
+
+**this.arr =>** this is the array of the array passed in without duplicates
+
+#### PowerSet methods
+
+**this.arrMethod("Method", "Argument") =>** use any array method by passing in a string for the method you desire to use and another string with the argument you want to pass.
+
+**this.isSuperset(PowerSet) =>** Returns true if the PowerSet is a superset of the PowerSet passed as an argument
+
+**this.union(PowerSet) =>** Returns a powerset that is a combonation of this PowerSet and the PowerSet passed in as an argument.
+
+**this.intersection(PowerSet) =>** returns PowerSet of elements in common between two PowerSets
+
+**this.symmetricDifference(PowerSet) =>** returns PowerSet of elements not shared by both PowerSets
+
+**this.difference(PowerSet) =>** returns PowerSet of elements not shared by this array with the passed in array
